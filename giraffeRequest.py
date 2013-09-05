a14d030083ab1c65dd0de0016394ac9f4c46838c
ganesshkumar <rpganesshkumar@gmail.com>
2013-09-05 23:00:53 +0530

    Adding giraffeRequest
    accepts mobileID and body of sms and splits it into required argumetns.
    Still need to consume it in the app.

---
+++ giraffeRequest.py
@@ -0,0 +1,20 @@
+
+class GiraffeRequest:
+
+    def __init__(self, mobile_identifier, body):
+        self.mobile_identifier = mobile_identifier
+        self.body = body
+        split_body(self)
+
+    def dump_request(self):
+        return "Mobile Identifier: " + self.mobile_identifier + " | Body: " + self.body
+
+    def split_body(self):
+        try:
+            parameters = self.body.split(' ', 2)
+            self.title = parameters[0]
+            self.amount = parameters[1]
+            self.user = parameters[2]
+            self.users = self.user.split(' ')
+        except:
+            raise ValueError("Error in parsing the body in GiraffeRequest")
